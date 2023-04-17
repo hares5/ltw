728f7a09af22cdaaba0ec4543f4063fea99f0445
federicOO1 <73711192+federicOO1@users.noreply.github.com>
2023-04-18 00:15:16 +0200

    Add files via upload

--- gruppi.php
+++ gruppi.php
@@ -7,6 +7,9 @@
     <link rel="stylesheet" href="homestyle.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
    
@@ -22,16 +25,16 @@
         <ul class="ml-auto">
         <l1><a href="crea_gruppo.php"><button class="btn btn-outline-secondary"><i class="fa fa-plus"></i> Crea nuovo gruppo</button></a></l1>
         <l1><a  href="pagina_login_gruppo.php"> <button class="btn btn-outline-secondary"><i class="fa fa-plus"></i> Unisciti ad un gruppo</button></a></l1>
-        <l1> 
-        <button class="btn btn-outline-primary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" id="dropdownbutton" aria-expanded="false"><i class="fa fa-user"></i> 
+        <l1 class="dropdown-container"> 
+        <button class="btn btn-outline-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-haspopup="true" id="dropdownbutton" aria-expanded="false"><i class="fa fa-user"></i> 
         <?php
         session_start();
         $user=$_SESSION["username"];
         $_SESSION["gruppo"]="";
         echo $user; ?><span class="caret"></span></button>
         <ul class="dropdown-menu" aria-labelledby="dropdownbutton">
-            <l1 class="dropdown-item" href="">Gestisci gruppi</l1>
-            <l1 class="dropdown-item" href="index.php"><i class="fa fa-sign-out"></i>Effettua logout</l1>
+            <l1><a class="dropdown-item" href="">Gestisci gruppi</a></l1>
+            <l1><a class="dropdown-item" href="index.php"><i class="fa fa-sign-out"></i>Effettua logout</a></l1>
         </ul>
         </div>
         </l1>
